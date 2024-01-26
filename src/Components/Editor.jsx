8585265419105ec54a2277240a086f05c9b01adc
Ahmed Gamal <agamal0077@gmail.com>
2024-01-26 10:27:23 +0200

    react markdown

--- src/Components/Editor.jsx
+++ src/Components/Editor.jsx
@@ -1,1 +1,15 @@
-export default function Editor() {}
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+export default function Editor() {
+  const [tap, setTap] = useState('write')
+  const [text, setText] = useState('')
+
+  return (
+    <div>
+      <button onClick={() => setTab('write')}>write</button>
+      <button onClick={() => setTab('preview')}>
+        preview
+      </button>
+    </div>
+  )
+}
