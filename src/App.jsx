c3da1a0a474916a5bdc9d3cb29c9ff58b354dc38
Ahmed Gamal <agamal0077@gmail.com>
2024-02-03 10:40:19 +0200

    render notes

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,34 @@ import './App.css'
 
 function App() {
   const [notes, setNotes] = useState([])
+  const [selectedNoteId, setSelectedNoteId] = useState(null)
+
+  const createNewNote = () => {
+    const newNote = {
+      id: Date.now(),
+      title: '',
+      content: '',
+    }
+    setNotes([...notes, newNote])
+    setSelectedNoteId(newNote.id)
+  }
+
+  const selectNote = (id) => {
+    selectedNoteId(id)
+  }
 
   return (
     <div className="app">
-      <Sidebar onAddNote={() => setNotes([...notes, {}])} />
-
+      <Sidebar onAddNote={createNewNote} />
+      <div className="noteList">
+        {notes.map((note) => (
+          <div
+            key={note.id}
+            className="noteTitle"
+            onClick={() => selectNote(note.id)}
+          ></div>
+        ))}
+      </div>
       <Editor />
     </div>
   )
